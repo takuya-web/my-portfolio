eead55f4acd50e79a0256fcebbb49a891afa3d76
Takuya Okamoto <t.okamoto@o-bisu.com>
2021-07-16 16:54:26 +0900

    20210716-hambergermenu-complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-$(function(){
-  $('.p-header__menu-button').on('click', function() {
-    $(this).toggleClass('active');
-    return false;
+//ハンバーガーメニュー&スライドメニュー
+jQuery(function(){
+  jQuery('.p-header__menu-button').on('click', function() {
+    jQuery(this).toggleClass('active');
+    jQuery('.p-header__nav').fadeToggle(100);
+    jQuery(".l-body__fade-layer").toggleClass('active');
   });
 });
