a33978291d4d726637a322c9b8b16b99e8f805ab
Takuya Okamoto <t.okamoto@o-bisu.com>
2021-07-16 22:52:16 +0900

    20210716-slick-complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,24 @@ jQuery(function(){
   jQuery('.p-header__menu-button').on('click', function() {
     jQuery(this).toggleClass('active');
     jQuery('.p-header__nav').fadeToggle(100);
-    jQuery(".l-body__fade-layer").toggleClass('active');
+    jQuery('.l-body__fade-layer').toggleClass('active');
   });
 });
+
+//Worksスリック
+jQuery(function() {
+  jQuery('.p-works__slick').slick( {
+    autoplay: true,
+    arrows: true,
+    slidesToShow: 2,
+    inifinite: true,
+  
+    responsive: [{
+      breakpoint: 390,
+        settings: {
+          slidesToShow: 1,
+          arrow: false,
+        }
+      }]
+    });
+  });
